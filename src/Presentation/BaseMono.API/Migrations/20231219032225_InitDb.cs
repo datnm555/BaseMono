202409db91a6c4237e24062cf57976aeb48555f0
datlouis <dat.louis@autumn.sg>
2023-12-19 15:30:41 +0700

    init

---
+++ src/Presentation/BaseMono.API/Migrations/20231219032225_InitDb.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaseMono.API.Migrations
+{
+    public partial class InitDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItem",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItem", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                column: "Id",
+                value: new Guid("2d5e543d-4cfc-42a8-a6fc-f564c5a44f85"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItem");
+        }
+    }
+}
